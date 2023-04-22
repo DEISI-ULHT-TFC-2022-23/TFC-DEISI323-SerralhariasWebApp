4bea843e7802bcacd65a82a13d8977a0e69c6e38
a21502898 <kiko.jffr@gmail.com>
2023-04-22 18:53:13 +0100

    Added different Product objects to the DB, Added login and Register links to home page

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('base', views.base_page_view, name = 'base'),
     path('home', views.home_page_view, name = 'home'),
+    path('base', views.base_page_view, name = 'base'),
     path('register', views.register_page_view, name = 'register'),
     path('login', views.login_page_view, name = 'login'),
     path('addProduct', views.add_product_page_view, name = 'addProduct'),
